ce936577421001bcbbe8e0cafe3551f8d9bfd891
Artem Kartavtsev <arkartavtsev@live.ru>
2019-03-15 18:27:04 +0700

    Добавление абстрактного класса

---
+++ src/component.js
@@ -0,0 +1,40 @@
+import {createElement} from './util';
+
+
+export default class Component {
+  constructor() {
+    if (new.target === Component) {
+      throw new Error(`Can't instantiate Component, only concrete one.`);
+    }
+
+    this._element = null;
+  }
+
+
+  get element() {
+    return this._element;
+  }
+
+  get template() {
+    throw new Error(`You have to define template.`);
+  }
+
+
+  addListeners() {}
+
+  removeListeners() {}
+
+
+  render() {
+    this._element = createElement(this.template);
+    this.addListeners();
+
+    return this._element;
+  }
+
+  unrender() {
+    this.removeListeners();
+    this._element.remove();
+    this._element = null;
+  }
+}
