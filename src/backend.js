ef4b104275b415b06702d1bf81e1ec80e44b0148
Artem Kartavtsev <arkartavtsev@live.ru>
2019-03-29 13:34:03 +0700

    Исправление адреса отправки запросов

--- src/backend.js
+++ src/backend.js
@@ -1,7 +1,7 @@
 import API from './api';
 
 
-const END_POINT = `https://es8-demo-srv.appspot.com/task-manager/`;
+const END_POINT = `https://es8-demo-srv.appspot.com/task-manager`;
 const AUTHORIZATION = `Basic eo0w590ik29889a1`;
 
 
